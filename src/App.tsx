1b9bce2d857e33313198e63106485d410daf19cb
Monte-Sina <sinamohamadi317@gmail.com>
2025-09-24 15:11:32 +0330

    Added graphql requests, charts to display result and utils functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
-import UserProfile from "./components/user/UserProfile";
-import SearchForm from "./components/form/SearchForm";
+import { useState } from 'react';
+import SearchForm from './components/form/SearchForm';
+import UserProfile from './components/user/UserProfile';
 
 const App = () => {
   const [userName, setUserName] = useState('quincylarson');
-
   return (
     <main className='mx-auto max-w-6xl px-8 py-20'>
       <SearchForm userName={userName} setUserName={setUserName} />
       <UserProfile userName={userName} />
     </main>
-  )
-}
-
-export default App
+  );
+};
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-// import Toaster component
-import { Toaster } from 'sonner';
-
+import { Toaster } from './components/ui/toaster.tsx';
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient.ts';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <ApolloProvider client={client}>
     <App />
     <Toaster />
-  </StrictMode>
+  </ApolloProvider>
 );
